3b34458c4340e0b1807c04b36485cbc4847a8431
zeMirco <mirco.zeiss@gmail.com>
2015-01-14 19:53:32 +0100

    init

---
+++ README.md
@@ -0,0 +1,37 @@
+
+# papertrail
+
+Send logs from your Golang apps to Papertrail.
+
+## Example
+
+```go
+package main
+
+import (
+  "log"
+  "github.com/zemirco/papertrail"
+)
+
+func main() {
+  writer := papertrail.Writer{
+    Port: 12345,
+    Network: papertrail.UDP,
+  }
+
+  // use writer directly
+  n, err := writer.Write([]byte("writer"))
+  if err != nil {
+    panic(err)
+  }
+  fmt.Printf("number of bytes written: %d\n", n)
+
+  // or create a new logger
+  logger := log.New(&writer, "", log.LstdFlags)
+  logger.Print("logger")
+}
+```
+
+## License
+
+MIT
---
+++ papertrail.go
@@ -0,0 +1,68 @@
+// Package papertrail sends logs to Papertrail (https://papertrailapp.com/).
+//
+// Implements io.Writer interface (http://golang.org/pkg/io/#Writer).
+//
+//  writer := papertrail.Writer{
+//    Port: 12345,
+//		Network: papertrail.UDP,
+//  }
+//
+//  // use writer directly
+//  n, err := writer.Write([]byte("writer"))
+//	if err != nil {
+//		panic(err)
+//	}
+//	fmt.Printf("number of bytes written: %d\n", n)
+//
+//  // or create a new logger
+//  logger := log.New(&writer, "", log.LstdFlags)
+//  logger.Print("logger")
+package papertrail
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"fmt"
+	"net"
+)
+
+const TCP = "tcp"
+const UDP = "udp"
+
+type Writer struct {
+	Port    int
+	Network string
+}
+
+func (w *Writer) Write(p []byte) (n int, err error) {
+
+	address := fmt.Sprintf("logs.papertrailapp.com:%d", w.Port)
+
+	if w.Network == UDP {
+		conn, err := net.Dial(UDP, address)
+		if err != nil {
+			return 0, err
+		}
+		defer conn.Close()
+		return conn.Write(p)
+	}
+
+	if w.Network == TCP {
+		roots := x509.NewCertPool()
+		ok := roots.AppendCertsFromPEM([]byte(pem))
+		if !ok {
+			return 0, errors.New("Failed to parse root certificate.")
+		}
+		conn, err := tls.Dial(TCP, address, &tls.Config{
+			RootCAs: roots,
+		})
+		if err != nil {
+			return 0, err
+		}
+		defer conn.Close()
+		return conn.Write(p)
+	}
+
+	return 0, errors.New("Invalid Protocol. Neither UDP nor TCP.")
+}
