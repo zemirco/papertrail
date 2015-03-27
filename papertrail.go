e56098f2d34a61b10d96c83f2565674530149585
ryanbillingsley <ryanbbillingsley@gmail.com>
2015-03-27 14:43:34 -0400

    Added a server parameter to the writer

--- papertrail.go
+++ papertrail.go
@@ -35,12 +35,13 @@ const (
 type Writer struct {
 	Port    int
 	Network string
+	Server  string
 }
 
 func (w *Writer) Write(p []byte) (n int, err error) {
 
 	var conn net.Conn
-	address := fmt.Sprintf("logs.papertrailapp.com:%d", w.Port)
+	address := fmt.Sprintf("%s.papertrailapp.com:%d", w.Server, w.Port)
 
 	switch w.Network {
 	case UDP:
